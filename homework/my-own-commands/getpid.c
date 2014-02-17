fdf4eaf227f5f4c0dc63dbd174e16d3f63248a51
mvazque <mvazque@mvazque-VirtualBox.(none)>
2014-02-17 10:05:51 -0800

    Completed basic code for all system calls

--- homework/my-own-commands/getpid.c
+++ homework/my-own-commands/getpid.c
@@ -1,10 +1,10 @@
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <dlfcn.h>
 #include <string.h>
 
 int main(int argc, char*argv[]) {
-	int pid = syscall(20);
-	char *message = "The PID for this process is ...";
-	syscall(4, 2, message, strlen(message));
-	syscall(4, 2, pid, strlen(pid));
+	pid_t pid = syscall(20);
+	printf("The PID for this process is ... %d \n", (int)pid);
 }
