7ce2e4340c3887e318276892846250270b0f07a6
mvazque <mvazque@mvazque-OptiPlex-390.(none)>
2014-03-31 10:16:44 -0700

    work done on personal shell. Start on README to answer questions given

--- homework/myshell/fork-exec.c
+++ homework/myshell/fork-exec.c
@@ -5,18 +5,23 @@
 #include <string.h>
 
 #define MAX_TOKENS 10
-
+#define MAX_COMMAND_CHAR 256
+#define MAX_DIRECTORY_CHAR 1024
+/**
+* This is meant to help tokenize the command the user inputs.
+* This allows for two word commands such as "cd .."
+*/
 int command_token (char *command, char *arguments[]) {
 	int index = 0;
+	/*list of characters to look out for and tokenize with*/
 	char *delimiter = " \n";
 	char *seperatedArguments = strtok(command, delimiter);
-
+	/*while loop will tokenize command and store it int array arguments*/
 	while(seperatedArguments != NULL){
 		arguments[index] = seperatedArguments;
 		seperatedArguments = strtok(NULL, delimiter);
 		index++;
 	}
-	
 	arguments[index] = NULL;
 	return 0;
 
@@ -25,34 +30,31 @@ int command_token (char *command, char *arguments[]) {
  * This program demonstrates the use of the fork() and exec()
  * functions.
  */
-int main(void) {
-
+int main() {
 	char *arguments[MAX_TOKENS];
-	char command[256];
-	char cwd[1024];
+	char command[MAX_COMMAND_CHAR];
+	char cwd[MAX_DIRECTORY_CHAR];
 	printf("Welcome to the custom shell. Have fun with it\n");
 	while(!feof(stdin)){
-    	   /* String to hold the command to run. */
 		int waitBoolean = 0;
+		/*these two lines will output the directory of the shell*/
        		getcwd(cwd, sizeof(cwd));
     		printf("%s$ ",cwd);
+		/*these next few commands get the input command and cleans it up a bit*/
 		fgets(command, sizeof(command), stdin);
 		command[strlen(command) - 1] = '\0';
-		
-		while (command[strlen(command) - 1] == ' ') {
-			command[strlen(command) - 1] = '\0';
-		}
+
+		/*This command searches for an & which is sign of being run in the background
+		  Also cleans up any excess spaces between '&' the commands*/
 		if (command[strlen(command) - 1] == '&') {
 			command[strlen(command) - 1] = '\0';
 			waitBoolean = 1;
-
-			/* Remove any extra spaces, just in case. */
-			while (command[strlen(command) - 1] == ' ') {
-				command[strlen(command) - 1] = '\0';
-			}
 		}
+		while (command[strlen(command) - 1] == ' ') {
+				command[strlen(command) - 1] = '\0';
+		}		
 		command_token(command, arguments);
-
+		/*Compares command to ensure it isn't a special case*/
 		if (strcmp("cd", arguments[0]) == 0) {
 			chdir(arguments[1]);
 		} else if (strcmp("secret-system-call", arguments[0]) == 0) {
@@ -81,5 +83,6 @@ int main(void) {
 		        }
 		}
 	}
+    printf("\n");
     return 0;
 }
