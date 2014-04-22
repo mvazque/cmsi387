e3d5bb7f6b2c8dfc2d8b336c1bfa6cc1f63773ed
mvazque <mvazque@mvazque-VirtualBox.(none)>
2014-04-21 19:09:27 -0700

    Initial Commit

---
+++ homework/paged-memory-address-translation/addressTranslation.c
@@ -0,0 +1,18 @@
+/**
+ * Implementation of the software paging unit.
+ */
+#include "addressTranslation.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static pagetable *ptr = NULL; // ptr = "page table register"
+
+void setPageTable(pagetable *pt) {
+    ptr = pt;
+}
+
+int getPhysical(int logical) {
+    // TODO
+}
+
