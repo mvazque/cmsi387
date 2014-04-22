af5520139dc66cde2207d90721a60cc2c848a3a4
mvazque <mvazque@mvazque-VirtualBox.(none)>
2014-04-21 20:07:54 -0700

    addressTranslation working fine

--- homework/paged-memory-address-translation/addressTranslation.c
+++ homework/paged-memory-address-translation/addressTranslation.c
@@ -12,7 +12,27 @@ void setPageTable(pagetable *pt) {
     ptr = pt;
 }
 
-int getPhysical(int logical) {
-    // TODO
+int getPhysical(int logical) {    
+   int page;
+   int offset;
+   int physicalAddress;
+   
+   //This checks to make sure that the logical address is valid
+   if(logical < 0 || logical >= 256){
+      return ERR_OUT_OF_RANGE;
+   }
+   
+   //Top four bits are the page number. Bottom four are the offset
+   page = ((logical & PAGEMASK) >> PAGEBITS);
+   offset = (logical & PAGESIZE);
+
+   //Checks table to ensure the frame is valid
+   if(ptr[page].valid == 0){
+      return  ERR_INVALID;
+   }
+
+   //Gets physical address of page and adds with offset
+   physicalAddress = ((ptr[page].frame) << PAGEBITS) + offset;
+   return physicalAddress;	
 }
 
