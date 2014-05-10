5bdde1be467c26881f3ac9b6cb95982262f622e1
dondi <dondi@lmu.edu>
2014-05-09 18:45:16 -0700

    Provide re-review feedback and update standards development report for HW 0422 resubmission.

--- homework/dining-philosophers/completeDiningPhilosophers.c
+++ homework/dining-philosophers/completeDiningPhilosophers.c
@@ -18,13 +18,17 @@ int philosopherStatus[MAX_NUMBER_PHILOSOPHERS];
 int chopstickStatus[MAX_NUMBER_PHILOSOPHERS];
 pthread_mutex_t chopsticks[MAX_NUMBER_PHILOSOPHERS];
 
-
 int randomwait(int bound) {
     int wait = rand() % bound;
     sleep(wait);
     return wait;
 }
 
+// JD: One last wrinkle here---multiple threads may be calling this
+//     concurrently, and so sometimes you will have overlapping
+//     philosopher printouts.  The fix to this is totally up our
+//     class's alley: another mutex!  The screen is a "shared resource"
+//     after all, it turns out.
 void printPhilosophers () {
    int i;
    for (i = 0; i < numberPhilosophers; i++) {
@@ -41,10 +45,21 @@ void printPhilosophers () {
 
 void chopstickGrab (int chopstick){
    pthread_mutex_lock(&chopsticks[chopstick]);
+   // JD: At this point, you are missing the required sanity-check
+   //     code to make sure that the chopstick really is available
+   //     before you grab it.  This will assure you whether or not
+   //     the mutex lock is working.  You should never see the error
+   //     with proper locking (just as with the bounded buffer sample
+   //     code), but once you remove the lock, the code should kick in.
    chopstickStatus[chopstick] += 1;
 }
 
 void chopstickRelease(int chopstick){
+   // JD: This function has two concerns: you should change the
+   //     chopstick state *before* you release the lock---because
+   //     otherwise you're outside of the critical section!  And like
+   //     the prior function, you need sanity check code here, before
+   //     you update the chopstick state.  Same deal as above.
    pthread_mutex_unlock(&chopsticks[chopstick]);
    chopstickStatus[chopstick] -= 1;
 }
@@ -74,6 +89,12 @@ void finishEating (int philosopher) {
    }
 }
 
+// JD: Your code spacing is hit-or-miss.  Sometimes you have a space
+//     after parentheses; sometimes you don't.  Sometimes there is a
+//     space before a left brace; sometimes not.  Sometimes you put a
+//     space after a keyword like if, while, or for; sometimes not.
+//     (for the latter, BTW, you *should* add a space, because if,
+//     while, and for are not functions)
 void* transitions(void* philosopher){
    int philosopherId = *(int*)philosopher;
    printPhilosophers();
