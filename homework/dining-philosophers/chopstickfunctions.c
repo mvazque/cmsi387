2df6718adc629d3e074ffcc10089f71171385409
dondi <dondi@lmu.edu>
2014-05-13 01:46:05 -0700

    Issue grade.

--- homework/dining-philosophers/chopstickfunctions.c
+++ homework/dining-philosophers/chopstickfunctions.c
@@ -4,12 +4,12 @@
 
 #include "utility.h"
 #include "table.h"
-//Attempt to grab chopstick will happen may end up waiting for this to happen
+
 void chopstickGrab (int chopstick){
    pthread_mutex_lock(&chopsticks[chopstick]);
    chopstickStatus[chopstick] += 1;
 }
-//Chopstick is released 
+
 void chopstickRelease(int chopstick){
    pthread_mutex_unlock(&chopsticks[chopstick]);
    chopstickStatus[chopstick] -= 1;
